121cbe0e4fc19181ec028c7bd1293e05d25ff4ba
hahavenn <hahavenn@gmail.com>
2024-01-21 17:02:44 +0300

    add navigation sections

---
+++ client/src/constants/navSections.js
@@ -0,0 +1,23 @@
+const SECTIONS_TITLE = {
+  SUBJECT: "Предметы",
+  SCIENCE_DIRECTIONS: "Научные направления",
+  JOB_PRACTICE: "Практики на предприятиях",
+  JOB_DEPARTMENT: "Работа на кафедре",
+  EXTRA_ACTIVITIES: "Внеурочная деятельность",
+  EMPLOYEES: "Сотрудники",
+  SCHEDULE: "Расписание",
+  DEPARTMENT_INFO: "Информация о кафедре",
+};
+
+const SECTIONS_DESCRIPTION = {
+  SUBJECT: "Дисциплины, которым обучают на кафедре",
+  SCIENCE_DIRECTIONS: "Темы НИР и диплома",
+  JOB_PRACTICE: "Предприятия для прохождения практики",
+  JOB_DEPARTMENT: "",
+  EXTRA_ACTIVITIES: "",
+  EMPLOYEES: "Список сотрудников, работающих на кафедре",
+  SCHEDULE: "Расписание отработок, дополнительных занятий",
+  DEPARTMENT_INFO: "Деятельность, дисциплины, связь с кафедрой",
+};
+
+export { SECTIONS_TITLE, SECTIONS_DESCRIPTION };
