0690249e135f33ecf6c58abc93201f54640c35e0
hahavenn <hahavenn@gmail.com>
2024-01-24 23:46:41 +0300

    remove missed_classes

--- client/src/router/routes.js
+++ client/src/router/routes.js
@@ -3,7 +3,6 @@ import employees_routes from "./employees";
 import extra_activities_routes from "./extra_activities";
 import job_department_routes from "./job_department";
 import job_practice_routes from "./job_practice";
-import missed_classes_routes from "./missed_classes";
 import schedule_routes from "./schedule";
 import science_directions_routes from "./science_directions";
 import subjects_routes from "./subjects";
@@ -14,7 +13,6 @@ const routes = [
   ...subjects_routes,
   ...science_directions_routes,
   ...schedule_routes,
-  ...missed_classes_routes,
   ...job_practice_routes,
   ...job_department_routes,
   ...extra_activities_routes,
