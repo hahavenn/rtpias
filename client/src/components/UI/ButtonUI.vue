585ce440bd1e34595a7e0c3bf6d48fea5f1e96ca
hahavenn <hahavenn@gmail.com>
2024-02-12 21:13:48 +0300

    create ButtonUI.vue

---
+++ client/src/components/UI/ButtonUI.vue
@@ -0,0 +1,36 @@
+<template>
+  <button>{{ props.placeholder }}</button>
+</template>
+
+<script setup>
+/* UI button */
+const props = defineProps({
+  placeholder: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<style scoped>
+button {
+  height: var(--input-height);
+  width: 100%;
+
+  outline: none;
+  border: var(--border-width-small) solid var(--color-default);
+  border-radius: var(--border-radius-default);
+
+  font-size: var(--font-size-default);
+  text-align: center;
+
+  &:focus {
+    border-color: var(--color-active-default);
+  }
+
+  &:hover {
+    border-color: var(--color-active-default);
+    cursor: pointer;
+  }
+}
+</style>
