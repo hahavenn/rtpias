222c6721b4e111bc313d85603e89eef75d93b318
hahavenn <hahavenn@gmail.com>
2024-04-08 02:05:00 +0300

    add: create time converter ms -> hh:mm:ss

---
+++ client/src/helpers/time.js
@@ -0,0 +1,12 @@
+// convert ms to hh:mm:ss
+export function convertMs_helper(ms) {
+  const seconds = Math.floor((ms / 1000) % 60);
+  const minutes = Math.floor((ms / (1000 * 60)) % 60);
+  const hours = Math.floor((ms / (1000 * 60 * 60)) % 24);
+
+  hours = hours < 10 ? "0" + hours : hours;
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  seconds = seconds < 10 ? "0" + seconds : seconds;
+
+  return `${hours}:${minutes}:${seconds}`;
+}
