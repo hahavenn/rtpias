3033aadbe5e39ee024458a5c66acfc95a7b8635d
hahavenn <hahavenn@gmail.com>
2024-04-09 20:53:38 +0300

    change: change helper function name

--- client/src/utils/logger.js
+++ client/src/utils/logger.js
@@ -1,12 +1,12 @@
 /* LOGGER UTIL */
 
-import { convertMs_helper } from "@/helpers/time";
+import { dateToTime_helper } from "@/helpers/time";
 
 export default async function logger_util(task_message) {
   // если у нас прод - не логируем
   if (import.meta.env.PROD) return;
 
   // фиксируем старт
-  const startedTime = convertMs_helper(new Date().getMilliseconds());
+  const startedTime = dateToTime_helper(new Date());
   console.log(`[${startedTime}] Task: ${task_message}`);
 }
