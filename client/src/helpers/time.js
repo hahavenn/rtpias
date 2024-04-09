192a29f2b228bf32a25132b5122835b95d41dc51
hahavenn <hahavenn@gmail.com>
2024-04-09 22:27:03 +0300

    update: add ms to dateToTime_helper

--- client/src/helpers/time.js
+++ client/src/helpers/time.js
@@ -3,10 +3,11 @@ export function dateToTime_helper(date) {
   let seconds = date.getSeconds();
   let minutes = date.getMinutes();
   let hours = date.getHours();
+  let ms = date.getMilliseconds();
 
   hours = hours < 10 ? "0" + hours : hours;
   minutes = minutes < 10 ? "0" + minutes : minutes;
   seconds = seconds < 10 ? "0" + seconds : seconds;
 
-  return `${hours}:${minutes}:${seconds}`;
+  return `${hours}:${minutes}:${seconds}.${ms}`;
 }
