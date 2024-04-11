bf7681f3a5261225d4bd4ca6989660c952435203
hahavenn <hahavenn@gmail.com>
2024-04-11 21:49:11 +0300

    update: improve Navigation_service

--- client/src/services/navigation.js
+++ client/src/services/navigation.js
@@ -1,20 +1,39 @@
 /* NAVIGATION SERVICE */
+import logger_util from "@/utils/logger";
 
 class Navigation_service {
   // эндпойнты для навигации
   static #ENDPOINTS = {
-    BASE_URL: "navigation",
+    BASE_URL: `${import.meta.env.VITE_SERVER_URL}/navigation`,
+  };
+
+  // сообщения для логгирования
+  static #LOG_MESSAGES = {
+    FETCH_ALL: "Запрос навигационных секций",
   };
 
   // получить список секций навигации
   static async getNavigationList() {
-    const response = (
-      await fetch(`${import.meta.env.VITE_SERVER_URL}/${this.#ENDPOINTS.BASE_URL}`, {
-        method: "GET",
-      })
-    ).json();
+    try {
+      logger_util(this.#LOG_MESSAGES.FETCH_ALL);
 
-    return response;
+      const response = await fetch(this.#ENDPOINTS.BASE_URL, {
+        method: "GET",
+      });
+
+      // если не ок
+      if (!response.ok) throw response;
+
+      logger_util(this.#LOG_MESSAGES.FETCH_ALL, { done: true });
+      return response.json();
+    } catch (response) {
+      logger_util(this.#LOG_MESSAGES.FETCH_ALL, {
+        error: true,
+        response,
+      });
+
+      return null;
+    }
   }
 }
 
