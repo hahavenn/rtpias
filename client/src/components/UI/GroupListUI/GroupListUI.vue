5186c058f294b00c4b71ba0fc559271255f427ca
hahavenn <hahavenn@gmail.com>
2024-04-15 00:41:00 +0300

    add: create GroupListUI.vue

---
+++ client/src/components/UI/GroupListUI/GroupListUI.vue
@@ -0,0 +1,70 @@
+<template>
+  <ul class="groups_list">
+    <template v-for="(group, group_index) in groupsObject">
+      <li class="group">
+        <span class="group_name">{{ group_index.toUpperCase() }}</span>
+        <InstancesList :group="group" />
+      </li>
+    </template>
+  </ul>
+</template>
+
+<script setup>
+import InstancesList from "./InstancesList/InstancesList.vue";
+
+const props = defineProps({
+  groupsObject: {
+    /*
+      {
+        "group_name": [
+          {
+            NAME: "имя",
+            LINK?: "ссылка",
+            ...
+          },
+          ...
+        ],
+        ...
+      }
+    */
+    type: Object,
+    required: true,
+  },
+});
+</script>
+
+<style scoped>
+.groups_list {
+  height: 100%;
+
+  --offset: 20px;
+
+  .group {
+    padding: calc(var(--offset) / 2) 0;
+    box-sizing: content-box;
+
+    &:not(:first-child) {
+      border-top: var(--v1-border-width_small) solid var(--v1-color_active_1);
+    }
+
+    display: flex;
+    flex-direction: row;
+
+    .group_name {
+      display: inline-block;
+      width: 65px;
+
+      color: var(--v1-color_blue_default);
+      font-size: var(--v1-font-size_big);
+      font-weight: var(--v1-font-weight_bold);
+      line-height: var(--v1-font-size_big);
+
+      padding: 0 var(--offset);
+
+      border-right: var(--v1-border-width_small) solid var(--v1-color_default_1);
+
+      flex-shrink: 0;
+    }
+  }
+}
+</style>
---
+++ client/src/components/UI/GroupListUI/InstancesList/InstancesList.vue
@@ -0,0 +1,60 @@
+<template>
+  <ul class="instances_list">
+    <template v-for="instance in group">
+      <li class="instance">
+        <RouterLink
+          v-if="instance?.LINK"
+          :to="instance?.LINK"
+        >
+          {{ instance.NAME }}
+        </RouterLink>
+        <span v-else>{{ instance.NAME }}</span>
+      </li>
+    </template>
+  </ul>
+</template>
+
+<script setup>
+const props = defineProps({
+  group: {
+    type: Array,
+    required: true,
+  },
+});
+</script>
+
+<style scoped>
+.instances_list {
+  --offset: 20px;
+
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: flex-start;
+
+  padding: 0 var(--offset);
+
+  .instance {
+    a,
+    span {
+      display: inline-block;
+
+      max-width: 100%;
+
+      text-overflow: ellipsis;
+      overflow: hidden;
+
+      font-size: var(--v1-font-size_medium);
+
+      @media (hover: hover) {
+        &:hover {
+          text-decoration: none;
+          color: var(--v1-color_red_default);
+          transition: var(--v1-transition_default);
+        }
+      }
+    }
+  }
+}
+</style>
