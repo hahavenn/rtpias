4ffa02ebe446f5c7845a9d629944336e600f01ca
hahavenn <hahavenn@gmail.com>
2024-04-15 00:41:49 +0300

    update: show full list of subjects

--- client/src/views/subjects/SubjectsView.vue
+++ client/src/views/subjects/SubjectsView.vue
@@ -1,5 +1,40 @@
-<template>subjects</template>
+<template>
+  <GroupListUI :groupsObject="groupedSubjects" />
+</template>
 
-<script setup></script>
+<script setup>
+import { onMounted, ref, computed } from "vue";
+
+import GroupListUI from "@/components/UI/GroupListUI/GroupListUI.vue";
+
+import { SUBJECTS_ERRORS } from "@/constants/errors";
+
+import alphabetSort_helper from "@/helpers/alphabetSort.js";
+
+import useSubjectsStore from "@/stores/useSubjectsStore.js";
+const subjectsStore = useSubjectsStore();
+
+// все предметы
+const subjects = ref([]);
+//загружаем предметы
+async function loadSubjects() {
+  subjects.value = await subjectsStore.getSubjects();
+
+  // не получилось загрузить
+  if (subjects.value.length === 0) {
+    console.log(SUBJECTS_ERRORS.GET_ALL);
+    return;
+  }
+}
+
+// сгруппированные предметы
+const groupedSubjects = computed(() => {
+  return subjects.value ? alphabetSort_helper(subjects.value, "NAME", true) : [];
+});
+
+onMounted(async () => {
+  await loadSubjects();
+});
+</script>
 
 <style scoped></style>
