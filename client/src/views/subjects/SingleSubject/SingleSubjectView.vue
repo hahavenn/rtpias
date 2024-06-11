bebf5511f99de35d18558393951a1327e1a9877a
hahavenn <hahavenn@gmail.com>
2024-06-11 20:27:39 +0300

    add: single subject component

---
+++ client/src/views/subjects/SingleSubject/SingleSubjectView.vue
@@ -0,0 +1,17 @@
+<template>single subject -> {{ subject_id }}</template>
+
+<script setup>
+import { computed } from "vue";
+import { useRoute } from "vue-router";
+
+import { SINGLE_SUBJECT_PARAMS } from "@/router/subjects/single_subject/single_subject";
+
+const route = useRoute();
+
+// получаем id из параметров марщрута
+const subject_id = computed(() => {
+  return route.params?.[SINGLE_SUBJECT_PARAMS.SUBJECT_ID];
+});
+</script>
+
+<style scoped></style>
